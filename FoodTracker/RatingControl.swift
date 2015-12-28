0222512ab627aaedee317da40e5c86c545ceefbc
Preeti Patel <preeti@preetis-macbook-pro.local>
2015-12-28 16:43:08 -0500

    Committing the third Checkpoint in the "Implement a Custom Control" lesson.

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -9,14 +9,21 @@
 import UIKit
 
 class RatingControl: UIView {
+  
+  // MARK: Properties
+  var rating = 0
+  var ratingButtons = [UIButton]()
 
   // MARK: Initialization
   required init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
-    let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
-    button.backgroundColor = UIColor.redColor()
-    button.addTarget(self, action: "ratingButtonTapped:", forControlEvents: .TouchDown)
-    addSubview(button)
+    for _ in 0..<5 {
+      let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+      button.backgroundColor = UIColor.redColor()
+      button.addTarget(self, action: "ratingButtonTapped:", forControlEvents: .TouchDown)
+      ratingButtons += [button]
+      addSubview(button)
+    }
   }
 
   override func intrinsicContentSize() -> CGSize {
