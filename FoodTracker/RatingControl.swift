002cb0bfb7dbc56622833748cca837e10eb7c999
Preeti Patel <preeti@preetis-macbook-pro.local>
2015-12-28 16:58:14 -0500

    Committing the fourth Checkpoint in the "Implement a Custom Control" lesson.

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -29,6 +29,15 @@ class RatingControl: UIView {
   override func intrinsicContentSize() -> CGSize {
     return CGSize(width: 240, height: 44)
   }
+  
+  override func layoutSubviews() {
+    var buttonFrame = CGRect(x: 0, y:0, width: 44, height: 44)
+    // Offset each button's origin by the length of the button plus spacing.
+    for (index, button) in ratingButtons.enumerate() {
+      buttonFrame.origin.x = CGFloat(index * (44 + 5))
+      button.frame = buttonFrame
+    }
+  }
 
   // MARK: Button Action
   func ratingButtonTapped(button: UIButton) {
