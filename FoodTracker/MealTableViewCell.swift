1d504b79d5d13068fa70ad635c598c41246dfc68
Preeti Patel <preeti@preetis-macbook-pro.local>
2015-12-31 15:29:55 -0500

    Committing the second Checkpoint in the "Create a Table View" lesson.

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Preeti Patel on 2015-12-31.
+//  Copyright © 2015 Jayesh Patel. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
