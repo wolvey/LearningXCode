d825b53d94f1e606869e1d3f1b78c59888feb4ba
Preeti Patel <preeti@preetis-macbook-pro.local>
2015-12-31 18:16:05 -0500

    Committing the third Checkpoint in the "Create a Table View" lesson.

--- FoodTracker/MealTableViewCell.swift
+++ FoodTracker/MealTableViewCell.swift
@@ -10,15 +10,20 @@ import UIKit
 
 class MealTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+  // MARK: Properties
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var photoImageView: UIImageView!
+  @IBOutlet weak var ratingControl: RatingControl!
 
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
 
-        // Configure the view for the selected state
-    }
+  override func setSelected(selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+
+    // Configure the view for the selected state
+  }
 
 }
