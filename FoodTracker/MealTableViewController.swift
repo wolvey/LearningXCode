62cb35dcd2b5d2c0b49b162273c7d0ac971b949a
Preeti Patel <preeti@preetis-macbook-pro.local>
2015-12-31 19:21:34 -0500

    Committing the fourth Checkpoint in the "Create a Table View" lesson.

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -38,42 +38,42 @@ class MealTableViewController: UITableViewController {
   // MARK: - Table view data source
 
   override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-    // #warning Incomplete implementation, return the number of sections
-    return 0
+    return 1
   }
 
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    // #warning Incomplete implementation, return the number of rows
-    return 0
+    return meals.count
   }
 
-  /*
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-  let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-  // Configure the cell...
-
-  return cell
+    // Table view cells are reused and should be dequeued using a cell identifier.
+    let cellIdentifier = "MealTableViewCell"
+    let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! MealTableViewCell
+    // Fetches the appropriate meal for the data source layout.
+    let meal = meals[indexPath.row]
+    cell.nameLabel.text = meal.name
+    cell.photoImageView.image = meal.photo
+    cell.ratingControl.rating = meal.rating
+    return cell
   }
-  */
 
   /*
   // Override to support conditional editing of the table view.
   override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-  // Return false if you do not want the specified item to be editable.
-  return true
+    // Return false if you do not want the specified item to be editable.
+    return true
   }
   */
 
   /*
   // Override to support editing the table view.
   override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-  if editingStyle == .Delete {
-  // Delete the row from the data source
-  tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-  } else if editingStyle == .Insert {
-  // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-  }
+    if editingStyle == .Delete {
+      // Delete the row from the data source
+      tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+    } else if editingStyle == .Insert {
+      // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }
   }
   */
 
@@ -87,8 +87,8 @@ class MealTableViewController: UITableViewController {
   /*
   // Override to support conditional rearranging of the table view.
   override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-  // Return false if you do not want the item to be re-orderable.
-  return true
+    // Return false if you do not want the item to be re-orderable.
+    return true
   }
   */
 
@@ -97,8 +97,8 @@ class MealTableViewController: UITableViewController {
 
   // In a storyboard-based application, you will often want to do a little preparation before navigation
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-  // Get the new view controller using segue.destinationViewController.
-  // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
   }
   */
 
