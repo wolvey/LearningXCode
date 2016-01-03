5d6451f8c39995877ce3bcf210f6c25c4d964649
Preeti Patel <preeti@preetis-macbook-pro.local>
2016-01-03 01:40:41 -0500

    Committing the second Checkpoint in the "Persist Data" lesson.

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -19,8 +19,12 @@ class MealTableViewController: UITableViewController {
     // Use the edit button item provided by the table view controller.
     navigationItem.leftBarButtonItem = editButtonItem()
 
-    // Load the sample data.
-    loadSampleMeals()
+    // Load any saved meals, otherwise load the sample data.
+    if let savedMeals = loadMeals() {
+      meals += savedMeals
+    } else {
+      loadSampleMeals()
+    }
   }
 
   func loadSampleMeals() {
@@ -72,6 +76,8 @@ class MealTableViewController: UITableViewController {
         meals.append(meal)
         tableView.insertRowsAtIndexPaths([newIndexPath], withRowAnimation: .Bottom)
       }
+      // Save the meals.
+      saveMeals()
     }
   }
 
@@ -86,6 +92,7 @@ class MealTableViewController: UITableViewController {
     if editingStyle == .Delete {
       // Delete the row from the data source
       meals.removeAtIndex(indexPath.row)
+      saveMeals()
       tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
     } else if editingStyle == .Insert {
       // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
@@ -124,5 +131,16 @@ class MealTableViewController: UITableViewController {
     }
   }
 
+  // MARK: NSCoding
 
+  func saveMeals() {
+    let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(meals, toFile: Meal.ArchiveURL.path!)
+    if !isSuccessfulSave {
+      print("Failed to save meals...")
+    }
+  }
+
+  func loadMeals() -> [Meal]? {
+    return NSKeyedUnarchiver.unarchiveObjectWithFile(Meal.ArchiveURL.path!) as? [Meal]
+  }
 }
